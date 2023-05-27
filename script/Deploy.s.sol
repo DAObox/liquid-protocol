89244bddb5a0377b16a6d0766d6e220c73dfa5c3
pythonpete32 <aaron@blackscale.io>
2023-05-27 19:03:55 +0400

    🚀 Setup deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.17;
 
-import { Foo } from "../src/Foo.sol";
+import { ContinuousDaoSetup } from "../src/core/ContinuousDaoSetup.sol";
 
 import { BaseScript } from "./Base.s.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract Deploy is BaseScript {
-    function run() public broadcaster returns (Foo foo) {
-        foo = new Foo();
+    function run() public broadcaster returns (ContinuousDaoSetup setup) {
+        setup = new ContinuousDaoSetup();
     }
 }
